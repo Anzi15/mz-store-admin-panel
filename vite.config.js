e6b0a27a01dca526095a8d82a1ffdd2d32c9ff9c
Nadeem Ahmed Channa <44109538+nadeemahmedchanna@users.noreply.github.com>
2025-09-20 00:17:08 -0700

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-    sourcemap: true,
-    chunkSizeWarningLimit: 2000
-  },
   optimizeDeps: {
     include: [
       'prosemirror-model',
